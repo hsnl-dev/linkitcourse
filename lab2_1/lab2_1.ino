c6f43b5f3b54d165915910663e7c91789ba001d1
Andy Wen <a409052003@gmail.com>
2016-04-10 10:28:57 +0800

    Added files via upload

---
+++ lab2_1/lab2_1.ino
@@ -0,0 +1,32 @@
+#include <Grove_LED_Bar.h>
+
+const int TouchPin=2;
+const int ledPin=13;
+const int ledBar=8;
+Grove_LED_Bar bar(9, 8, 0);  // Clock pin, Data pin, Orientation
+
+void setup() {
+  bar.begin();
+  pinMode(TouchPin, INPUT);
+  pinMode(ledPin,OUTPUT);
+} 
+ 
+void loop() {
+  int sensorValue = digitalRead(TouchPin);
+  
+  if(sensorValue==0){
+    digitalWrite(ledPin,HIGH);
+    bar.setLevel(10);
+  }
+  else{
+    digitalWrite(ledPin,LOW);
+    // Walk through the levels
+    for (int i = 0; i <= 10; i++){
+      if (digitalRead(TouchPin)==0){         // sensor detect
+        break;
+      }
+      bar.setLevel(i);
+      delay(100);
+    } 
+  }
+}
